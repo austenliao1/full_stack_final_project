4131039984778ee34e419ec09423d17718135f70
Will Cai <wil067@berkeley.edu>
2023-04-23 16:11:38 -0700

    add ingredient list

--- client/src/Components/RecipeGrid.jsx
+++ client/src/Components/RecipeGrid.jsx
@@ -10,18 +10,23 @@ function RecipeGrid({ recipes }) {
     <Container fluid="md">
       <Row>
         <>
-          {recipes.map((r) => (
-            <Col key={r.id}>
-              <RecipeCard
-                sourceUrl={r.sourceUrl}
-                image={r.image}
-                dishType={r.dishTypes[0] && r.dishTypes[0].toUpperCase()}
-                title={r.title}
-                readyInMinutes={r.readyInMinutes}
-                aggregateLikes={r.aggregateLikes}
-              />
-            </Col>
-          ))}
+          {recipes.map((r) => {
+            const ingredients = r.extendedIngredients.map((i) => i.original);
+
+            return (
+              <Col key={r.id}>
+                <RecipeCard
+                  sourceUrl={r.sourceUrl}
+                  image={r.image}
+                  dishType={r.dishTypes[0] && r.dishTypes[0].toUpperCase()}
+                  title={r.title}
+                  readyInMinutes={r.readyInMinutes}
+                  aggregateLikes={r.aggregateLikes}
+                  ingredients={ingredients}
+                />
+              </Col>
+            );
+          })}
         </>
       </Row>
     </Container>
