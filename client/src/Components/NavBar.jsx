2b68661c58de2e5addbb9117ec00436f35a73edc
Jackie Pei <jackiew@berkeley.edu>
2023-04-23 18:18:56 -0700

    create NavBar

---
+++ client/src/Components/NavBar.jsx
@@ -0,0 +1,21 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function NavBar() {
+  return (
+    <Navbar bg="light" expand="lg" sticky="top">
+      <Container>
+        <Navbar.Brand href="#home">Foodstagram</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#home">Saved</Nav.Link>
+            <Nav.Link href="#link">Profile</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
